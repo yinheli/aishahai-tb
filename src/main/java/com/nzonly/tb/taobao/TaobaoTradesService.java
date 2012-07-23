ebfcf91d9eb9e0bbfc6117e8b31959b82ba63095
yinheli <yinheli@gmail.com>
2012-07-24 07:31:38 +0800

    添加实时获取订单方法

--- src/main/java/com/nzonly/tb/taobao/TaobaoTradesService.java
+++ src/main/java/com/nzonly/tb/taobao/TaobaoTradesService.java
@@ -4,18 +4,23 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.apache.commons.beanutils.BeanUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Component;
 
 import com.nzonly.tb.entity.TaobaoTask;
+import com.nzonly.tb.entity.TaobaoTrade;
 import com.nzonly.tb.taobao.exception.TaobaoApiException;
 import com.nzonly.tb.taobao.exception.TaobaoException;
 import com.taobao.api.ApiException;
 import com.taobao.api.request.TopatsResultGetRequest;
 import com.taobao.api.request.TopatsTradesSoldGetRequest;
 import com.taobao.api.request.TradeFullinfoGetRequest;
+import com.taobao.api.request.TradesSoldGetRequest;
 import com.taobao.api.response.TopatsResultGetResponse;
 import com.taobao.api.response.TopatsTradesSoldGetResponse;
 import com.taobao.api.response.TradeFullinfoGetResponse;
+import com.taobao.api.response.TradesSoldGetResponse;
 
 /**
  * 淘宝交易操作
@@ -27,6 +32,45 @@ import com.taobao.api.response.TradeFullinfoGetResponse;
  */
 @Component
 public class TaobaoTradesService extends TaobaoService {
+	
+	/**
+	 * 搜索当前会话用户作为卖家已卖出的交易数据
+	 * 
+	 * <p>
+	 * 此方法总是查询淘宝 api 数据, 较慢.
+	 * 查询完毕后更新/创建 Trade 数据到本地数据库
+	 * </p>
+	 * 
+	 * <p> 该接口为实时获取数据, 需要分页</p>
+	 *  
+	 * @param start 起始时间
+	 * @param end 结束时间
+	 * @param pageRequest 分页请求
+	 * @param session
+	 * @return
+	 * @throws TaobaoException
+	 * @author yinheli
+	 * @date 2012-7-24 上午7:09:06
+	 */
+	public Page<TaobaoTrade> tradesSoldGet(Date start, Date end, PageRequest pageRequest, String session) throws TaobaoException {
+		String fields = "tid, buyer_nick, payment, alipay_no, buyer_alipay_no, orders.title, orders.pic_path, orders.price";
+		TradesSoldGetRequest req = new TradesSoldGetRequest();
+		req.setFields(fields);
+		req.setStartCreated(start);
+		req.setEndCreated(end);
+		req.setPageNo((long)pageRequest.getPageNumber());
+		req.setPageSize((long)pageRequest.getPageSize());
+		try {
+			TradesSoldGetResponse resp = client.execute(req, session);
+			if (log.isDebugEnabled()) {
+				log.debug("trades:{}, \nall:{}", dump(resp.getTrades()), dump(resp));
+			}
+		} catch (ApiException e) {
+			throw new TaobaoApiException(e);
+		}
+		
+		return null;
+	}
 	
 	/**
 	 * 异步获取三个月内已卖出的交易详情
--- src/test/java/com/nzonly/tb/taobao/TaobaoTradesServiceTest.java
+++ src/test/java/com/nzonly/tb/taobao/TaobaoTradesServiceTest.java
@@ -1,9 +1,11 @@
 package com.nzonly.tb.taobao;
 
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.annotation.Rollback;
 
 import com.nzonly.tb.test.BaseTestNoWeb;
@@ -18,6 +20,22 @@ public class TaobaoTradesServiceTest extends BaseTestNoWeb implements TestConsta
 	
 	@Autowired
 	private TaobaoTradesService service;
+	
+	/**
+	 * 搜索当前会话用户作为卖家已卖出的交易数据 
+	 * 
+	 * @throws Exception
+	 * @author yinheli
+	 * @date 2012-7-24 上午7:23:44
+	 */
+	@Test
+	public void testTradesSoldGet() throws Exception {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		Date start = sdf.parse("2012-07-21 00:00:00");
+		Date end =  sdf.parse("2012-07-21 23:59:59");
+		PageRequest page = new PageRequest(1, 40);
+		service.tradesSoldGet(start, end, page, session);
+	}
 	
 	/**
 	 * 提交异步查询任务
